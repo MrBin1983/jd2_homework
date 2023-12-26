c8d6ab05b47183aa7d90e85b8698192ea4d7b94e
Mrbin1983 <mrbin1983@yandex.by>
2023-12-26 20:27:14 +0300

    Hibernate. Part one. UnitTest all.

--- hw11/src/test/java/it/dao/PersonDaoImplTest.java
+++ hw11/src/test/java/it/dao/PersonDaoImplTest.java
@@ -28,11 +28,28 @@ public class PersonDaoImplTest {
 
     @After
     public void tearDown() throws Exception {
+        Connection conn = TestConfiguration.getConnection();
+        conn.createStatement().executeUpdate(
+                "DELETE FROM T_PERSON;"
+        );
         personDao = null;
     }
 
     @Test
-    public void getPerson() {
+    public void getPerson() throws SQLException, ClassNotFoundException {
+        // Given
+        Connection conn = TestConfiguration.getConnection();
+        conn.createStatement().executeUpdate(
+                "insert into T_PERSON values (1, 25, 'Merlin', 'Monro');"
+        );
+        conn.close();
+        // When
+        Person person = personDao.getPerson(1);
+
+        // Then
+        assertEquals((Integer) 25, person.getAge());
+        assertEquals("Merlin", person.getName());
+        assertEquals("Monro", person.getSurname());
     }
 
     @Test
@@ -56,9 +73,35 @@ public class PersonDaoImplTest {
         rs.next();
         int actualCount = rs.getInt(1);
         assertEquals(1, actualCount);
+        conn.close();
     }
 
     @Test
-    public void deletePerson() {
+    public void deletePerson() throws SQLException, ClassNotFoundException {
+        // Given
+        Connection conn = TestConfiguration.getConnection();
+        conn.createStatement().executeUpdate(
+                "insert into T_PERSON values (1, 25, 'Merlin', 'Monro');"
+        );
+        ResultSet rs1 = conn.createStatement().executeQuery(
+                "select id from T_PERSON where NAME='Merlin' and SURNAME='Monro';"
+        );
+        rs1.next();
+        int actualCount1 = rs1.getInt(1);
+        assertEquals(1, actualCount1);
+        Person person = new Person(
+                1, 25, "Merlin", "Monro"
+        );
+        // When
+        personDao.deletePerson(person);
+        // Then
+
+        ResultSet rs2 = conn.createStatement().executeQuery(
+                "select count(*) from T_PERSON;"
+        );
+        rs2.next();
+        int actualCount2 = rs2.getInt(1);
+        assertEquals(0, actualCount2);
+        conn.close();
     }
 }
